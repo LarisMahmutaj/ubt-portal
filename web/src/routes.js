bffec1274f7edc0b1fc6cfc7db764165e9b14fb8
Laris Mahmutaj <laris.mahmutaj@gmail.com>
2020-06-07 00:55:51 +0200

    Added EmailConfirmed Page

--- web/src/routes.js
+++ web/src/routes.js
@@ -1,17 +1,22 @@
-import Home from './views/Home.vue';
-import Register from './views/Register.vue';
-import Login from './views/Login.vue';
-
+import Home from "./views/Home.vue"
+import Register from "./views/Register.vue"
+import Login from "./views/Login.vue"
+import EmailConfirmed from "./views/EmailConfirmed.vue"
 
 export const routes = [
   {
-    path: '/home',
-    name: 'home',
+    path: "/home",
+    name: "home",
     component: Home,
     meta: {
       requiresAuth: true,
     },
   },
-  { path: '/register', name: 'register', component: Register },
-  { path: '/', name: 'login', component: Login },
-];
+  { path: "/register", name: "register", component: Register },
+  { path: "/", name: "login", component: Login },
+  {
+    path: "/email-confirmed",
+    name: "emailConfirmed",
+    component: EmailConfirmed,
+  },
+]
---
+++ web/src/views/EmailConfirmed.vue
@@ -0,0 +1,46 @@
+<template>
+	<div class="container">
+		<div class="content">
+			<v-img class="image" src="../assets/images/confirmed.svg"></v-img>
+			<h1>Email Confirmed</h1>
+			<v-btn class="login-btn" color="#6c63ff" dark to="/" width="100px"
+				>Login</v-btn
+			>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: "EmailConfirmed"
+	};
+</script>
+
+<style scoped>
+	.container {
+		color: white;
+		margin: 0;
+		padding: 0;
+		min-width: 100%;
+		min-height: 100vh;
+		background-color: #185bde;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+
+	.image {
+		width: 500px;
+	}
+
+	h1 {
+		font-size: 25pt;
+		margin: 20px 0 10px 0;
+	}
+</style>
